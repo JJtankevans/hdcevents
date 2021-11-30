16d5f7312bff064d0e14acd04966e8c970842580
JJtankevans <v.oliveira042997@gmail.com>
2021-11-30 10:45:42 -0400

    Adicionando a visualização dos eventos que estou participando na pagina de dashboard

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -75,7 +75,12 @@ class EventController extends Controller
 
         $events =  $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', [
+            'events' => $events, 
+            'eventsasparticipant' => $eventsAsParticipant]
+        );
     }
 
     public function destroy($id){
@@ -87,7 +92,13 @@ class EventController extends Controller
 
     public function edit($id){
         
+        $user =  auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user->id){
+            return redirect('/dashboard');
+        }
 
         return view('Events.edit',['event' => $event]);
     }
--- resources/views/Events/dashboard.blade.php
+++ resources/views/Events/dashboard.blade.php
@@ -23,7 +23,7 @@
                     <tr>
                         <td scope="row">{{ $loop->index + 1 }}</td>
                         <td><a href="/events/{{ $item->id }}">{{ $item->title }}</a></td>
-                        <td>0</td>
+                        <td>{{ count($item->users) }}</td>
                         <td>
                             <a href="/events/edit/{{ $item->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a>
                             <form action="/events/{{ $item->id }}" method="POST">
@@ -40,5 +40,35 @@
         <p>Você ainda não tem eventos, <a href="/events/create">criar evento</a></p>
     @endif
 </div>
-
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Eventos que estou participando</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($eventsasparticipant) > 0)
+    <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Participantes</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($eventsasparticipant as $item)
+                    <tr>
+                        <td scope="row">{{ $loop->index + 1 }}</td>
+                        <td><a href="/events/{{ $item->id }}">{{ $item->title }}</a></td>
+                        <td>{{ count($item->users) }}</td>
+                        <td>
+                           <a href="#">Sair do evento</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Você ainda não está participando de nnhum evento, <a href="/">veja todos os eventos</a></p>
+    @endif
+</div>
 @endsection
